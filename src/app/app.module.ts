7db744a13242114bb54a2d87e4c6191d3cbea034
Abuthair <Sheikabuthair.pc@gmail.com>
2023-07-01 23:40:02 +0530

    Day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { TechStackComponent } from './Components/tech-stack/tech-stack.component';
+import { AboutComponent } from './Components/about/about.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TechStackComponent,
+    AboutComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
